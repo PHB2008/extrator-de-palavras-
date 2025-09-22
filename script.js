74f4486466d74d33ef60e165a940f95a411b0e59
PHB2008 <pedro.biolchi@escola.pr.gov.br>
2025-09-22 10:47:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click" , MostraPalavrasChave);
 function MostraPalavrasChave(){
- const texto = document.querySelector("#entrada-de-texto").value;
+const texto = document.querySelector("#entrada-de-texto").value;
 const campoResultado = document.querySelector("#resultado-palavrachave");
 campoResultado.textContent = texto;
-const palavras = texto.split(" ");
+const palavrasChave = processaTexto(texto);
 campoResultado.textContent = palavras.join(" , ");
+}
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+    for (let i of palavras){
+        frequencias [i] = 0;
+
+        for (let j of palavras){
+            if ( i == j){
+                frequencias[i]++;
+            }
+        }
+    }
+console.log(frequencias);
+    return palavras;
 }
